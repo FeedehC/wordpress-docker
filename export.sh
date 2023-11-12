4c11fe90d4208c7ec4f5953cf4d85e76dab7861d
Ubuntu <ubuntu@ubuntu.fibertel.com.ar>
2023-11-12 17:58:22 +0000

    feat: export.sh for database easy exports

---
+++ export.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+_os="`uname`"
+_now=$(date +"%m_%d_%Y")
+_file="wp-data/data_$_now.sql"
+
+# Make directory
+mkdir -p wp-data
+
+# Export dump
+EXPORT_COMMAND='exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"'
+docker-compose exec wpdb sh -c "$EXPORT_COMMAND" > $_file
+
+if [[ $_os == "Darwin"* ]] ; then
+  sed -i '.bak' 1,1d $_file
+else
+  sed -i 1,1d $_file # Removes the password warning from the file
+fi
